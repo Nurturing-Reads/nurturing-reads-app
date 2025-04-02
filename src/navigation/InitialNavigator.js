0dfb94c734d8cbf5903ecaf66a4fb20e2923c4ad
Chris Chow <chitlchow@gmail.com>
2025-04-02 14:43:20 +0100

    Update backend for macOS

--- src/navigation/InitialNavigator.js
+++ src/navigation/InitialNavigator.js
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import HomeNavigator from "./HomeNavigator";
 import StudentScreen from "../screens/studentScreen";
 import {WelcomeScreen} from "../screens/WelcomeScreen";
+import BookScreen from "../screens/bookScreen";
 
 // Todos: Making a
 const InitStackNav = createNativeStackNavigator();
@@ -11,7 +12,7 @@ const InitStack = () => {
     return (
         <InitStackNav.Navigator initialRouteName={"Home"}>
             <InitStackNav.Screen name="Home" component={WelcomeScreen} />
-            <InitStackNav.Screen name="Student" component={StudentScreen} />
+            <InitStackNav.Screen name="Student" component={BookScreen} />
             <InitStackNav.Screen name="Parent" component={HomeNavigator} />
         </InitStackNav.Navigator>
     )
